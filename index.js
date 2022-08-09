67b6cdabae95c83fab98cffeacd812a59e653bd6
ClaraPons <claragapons@gmail.com>
2022-08-09 09:47:06 +0200

    Got finish

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const port = 5001
 
-const gotCharacterRoutes = require('./routes/json.js')
+const gotCharacterRoutes = require('./routes/Got.js')
 
 app.use('/games-of-thrones', gotCharacterRoutes)
 
